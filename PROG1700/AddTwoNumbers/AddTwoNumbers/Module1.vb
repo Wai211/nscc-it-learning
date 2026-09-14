90b85133e3449acbdfc893af97adecd1ce1ae241
Wai211 <328208968+Wai211@users.noreply.github.com>
2026-09-14 16:58:18 -0300

    Add PROG1700 AddTwoNumbers project

---
+++ PROG1700/AddTwoNumbers/AddTwoNumbers/Module1.vb
@@ -0,0 +1,23 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim num1 As Integer
+        Dim num2 As Integer
+        Dim answer As Integer
+
+        Console.Write("Type a number and press enter.")
+        num1 = Console.ReadLine
+
+        Console.Write("Type another number to add to it and press Enter.")
+        num2 = Console.ReadLine
+
+        answer = num1 + num2
+
+        Console.WriteLine("The answer is " & answer)
+        Console.Write("Press any Key to Continue")
+
+        Console.ReadKey(True)
+
+    End Sub
+
+End Module
---
+++ PROG1700/AverageThreeNumbers/AverageThreeNumbersUpdated/Module1.vb
@@ -0,0 +1,24 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim number1, number2, number3 As Double
+        Dim answer As Double
+
+        Console.Write("Type your 1st number and press enter: ")
+        number1 = Console.ReadLine
+
+        Console.Write("Type your 2nd number and press enter: ")
+        number2 = Console.ReadLine
+
+        Console.Write("Type your 3rd number and press enter: ")
+        number3 = Console.ReadLine
+
+        answer = (number1 + number2 + number3) / 3
+        Console.WriteLine($"The Average of {number1} and {number2} and {number3} is {answer}")
+
+        Console.ReadKey(True)
+
+
+    End Sub
+
+End Module
